8fa7060894a3b6537a5fa60c385a40d4135bae08
jiyun <jiyun9762@gmail.com>
2025-05-04 18:01:52 +0900

    change system tray crate

--- build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    let _ = embed_resource::compile("icon.rc", embed_resource::NONE);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,125 @@
 // Prevents additional console window on Windows in release
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tray_item::{IconSource, TrayItem};
+use tray_icon::{
+    menu::{Menu, MenuEvent, MenuItem},
+    TrayIcon, TrayIconBuilder, TrayIconEvent,
+};
+use winit::{application::ApplicationHandler, event_loop::EventLoop};
+
+#[derive(Debug)]
+enum UserEvent {
+    TrayIconEvent(tray_icon::TrayIconEvent),
+    MenuEvent(tray_icon::menu::MenuEvent),
+}
+
+struct Application {
+    tray_icon: Option<TrayIcon>,
+    quit_tray_item: Option<MenuItem>,
+}
+
+impl Application {
+    fn new() -> Application {
+        Application {
+            tray_icon: None,
+            quit_tray_item: None,
+        }
+    }
+
+    fn new_tray_icon(&mut self) -> TrayIcon {
+        let path = concat!(env!("CARGO_MANIFEST_DIR"), "/resources/app-icon.ico");
+        let icon = load_icon(std::path::Path::new(path));
+
+        TrayIconBuilder::new()
+            .with_menu(Box::new(self.new_tray_menu()))
+            .with_icon(icon)
+            .with_title("netflix-skip-intro")
+            .build()
+            .unwrap()
+    }
+
+    fn new_tray_menu(&mut self) -> Menu {
+        let menu = Menu::new();
+        let item1 = MenuItem::new("item1", true, None);
+        let quit_item = MenuItem::new("종료", true, None);
+
+        if let Err(err) = menu.append_items(&[&quit_item, &item1]) {
+            println!("{err:?}");
+        }
+
+        self.quit_tray_item = Some(quit_item);
+
+        menu
+    }
+}
+
+impl ApplicationHandler<UserEvent> for Application {
+    fn resumed(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        _event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        _event: winit::event::WindowEvent,
+    ) {
+    }
+
+    fn new_events(
+        &mut self,
+        _event_loop: &winit::event_loop::ActiveEventLoop,
+        cause: winit::event::StartCause,
+    ) {
+        if winit::event::StartCause::Init == cause {
+            self.tray_icon = Some(self.new_tray_icon());
+        }
+    }
+
+    fn user_event(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop, event: UserEvent) {
+        println!("{event:?}");
+        match event {
+            UserEvent::MenuEvent(menu_event) => {
+                if let Some(quit_item) = &self.quit_tray_item {
+                    if quit_item.id() == menu_event.id() {
+                        std::process::exit(0);
+                    }
+                }
+            }
+            UserEvent::TrayIconEvent(_) => {}
+        }
+    }
+}
 
 fn main() {
-    let mut tray = TrayItem::new("My App", IconSource::Resource("appicon")).unwrap();
+    let event_loop = EventLoop::<UserEvent>::with_user_event().build().unwrap();
 
-    tray.add_menu_item("종료", || {
-        std::process::exit(0);
-    })
-    .unwrap();
+    // set a tray event handler that forwards the event and wakes up the event loop
+    let proxy = event_loop.create_proxy();
+    TrayIconEvent::set_event_handler(Some(move |event| {
+        proxy.send_event(UserEvent::TrayIconEvent(event));
+    }));
+    let proxy = event_loop.create_proxy();
+    MenuEvent::set_event_handler(Some(move |event| {
+        proxy.send_event(UserEvent::MenuEvent(event));
+    }));
 
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
+    let mut app = Application::new();
+
+    let menu_channel = MenuEvent::receiver();
+    let tray_channel = TrayIconEvent::receiver();
+
+    if let Err(err) = event_loop.run_app(&mut app) {
+        println!("Error: {:?}", err);
     }
+}
+
+fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
 }
